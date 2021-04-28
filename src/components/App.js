967a6729fc626139534fa50097e95c9f8a0a2592
Denis Frolov <denisfrolov.coder@gmail.com>
2021-04-28 18:27:24 +0300

    add state and data.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/App.css";
 
 import AddAppointments from "./AddAppointments";
 import SearchAppointments from "./SearchAppointments";
 import ListAppointments from "./ListAppointments";
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      appointment: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((result) => {
+        const apts = result.map((item) => {
+          return item;
+        });
+
+        this.setState({
+          appointment: apts,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
