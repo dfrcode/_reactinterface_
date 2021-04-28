9f1b21f3b3b16d9becaf3e1f0ee6a4ae23f9457c
Denis Frolov <denisfrolov.coder@gmail.com>
2021-04-28 18:02:21 +0300

    add components AddAppointments, ListAppointments and SearchAppointments

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import "jquery/dist/jquery.js";
 import "popper.js/dist/umd/popper.js";
 import "bootstrap/dist/js/bootstrap.js";
-import App from "./App";
+import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
+import "./css/index.css";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
